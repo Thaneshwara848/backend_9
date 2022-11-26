cb55695d1969723f55b4a9b2d43b73eda5970dea
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2022-11-26 19:39:27 +0530

    Demo1

---
+++ Demo1/src/main/java/abc/App.java
@@ -0,0 +1,36 @@
+package abc;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+
+	public static void main(String[] args) {
+//		<hibernate-configuration>
+//	    <session-factory name="demo">
+		
+		Configuration cfg= new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		
+		SessionFactory factroy=cfg.buildSessionFactory();
+		
+		Session session= factroy.openSession();
+		
+		org.hibernate.Transaction tx= session.beginTransaction();
+		
+		Employee e = new Employee();
+		e.setId(100);
+		e.setName("Thanesh");
+		e.setAge(25);
+		e.setSalary(50000);
+		e.setDesig("Trainer");
+		
+		session.save(e);
+		tx.commit();
+		
+		System.out.println("Data Inserted ...!");
+	}
+
+}
